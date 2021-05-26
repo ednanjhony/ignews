84124d7b53f1f61a1acf53ddb2511c1329630188
Ednan Jhony <ednanjhony@yahoo.com.br>
2021-05-26 07:51:06 -0300

    added tests

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -24,6 +24,8 @@ describe('ActiveLink component', () => {
 		 render(
 			<Header />
 		)
+
+		screen.logTestingPlaygroundURL()
 	
 		expect(screen.getByText('Home')).toBeInTheDocument()
 		expect(screen.getByText('Posts')).toBeInTheDocument()
